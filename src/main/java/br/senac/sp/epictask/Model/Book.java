a083ed5d369064dea997ca7fa3c85b6082c23bf0
Eduardo <ef3317707@gmail.com>
2023-11-28 20:45:05 -0300

    Estiliza todas as telas e adiciona os demais campos para cadastrar o livros

--- src/main/java/br/senac/sp/epictask/Model/Book.java
+++ src/main/java/br/senac/sp/epictask/Model/Book.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 //import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 //import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -19,7 +20,20 @@ public class Book {
 
     @NotBlank
     String title;
+
+    @NotBlank
+    String autor;
+
+    @NotNull
+    Long launch;
+
+    @NotBlank
+    String company;
+
+    @NotBlank
+    String language;
     
     @Size(min = 10, message =  "Digite uma descrição com pelo menos 10 caracteres")
     String description;
+
 }
